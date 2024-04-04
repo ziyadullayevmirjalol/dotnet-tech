f96e924f5f79d33ac68d59f1c74bc6ce7c94caa3
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-04 09:21:50 +0500

    Add project files.

---
+++ Interview/Algorthms/Sorting.cs
@@ -0,0 +1,64 @@
+﻿namespace Interview.Algorthms;
+
+public class Sorting
+{
+    public int[] bubbleSort(int[] arr)
+    {
+        int length = arr.Length;
+        while (length != 0)
+        {
+            int maxindex = 0;
+            for (int i = 1; i < length; i++)
+            {
+                if (arr[i - 1] > arr[i])
+                {
+                    int temp = arr[i - 1];
+                    arr[i - 1] = arr[i];
+                    arr[i] = temp;
+
+                    maxindex = i;
+                }
+            }
+            length = maxindex;
+        }
+        return arr;
+    }
+
+    public int[] selectionSort(int[] arr)
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            int minindex = i;
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[minindex] > arr[j])
+                {
+                    minindex = j;
+                }
+            }
+            if (minindex != i)
+            {
+                int temp = arr[minindex];
+                arr[minindex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        return arr;
+    }
+    
+    public int[] insertionSort(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int sorted = i - 1;
+            while (sorted > -1 && arr[sorted] > arr[sorted + 1])
+            {
+                int temp = arr[sorted];
+                arr[sorted] = arr[sorted + 1];
+                arr[sorted + 1] = temp;
+                sorted--;
+            }
+        }
+        return arr;
+    }
+}
---
+++ Interview/Program.cs
@@ -0,0 +1,10 @@
+﻿using Interview.Algorthms;
+
+Sorting sorting = new Sorting();
+
+foreach (var item in sorting.bubbleSort(new int[] { 10, 200, 30, 41, 52, 63, 7, 8, 93 }))
+    Console.WriteLine(item);
+foreach (var item in sorting.selectionSort(new int[] { 10, 200, 30, 41, 52, 63, 7, 8, 93 }))
+    Console.WriteLine(item);
+foreach (var item in sorting.insertionSort(new int[] { 10, 200, 30, 41, 52, 63, 7, 8, 93 }))
+    Console.WriteLine(item);
