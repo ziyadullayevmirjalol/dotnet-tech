5263a13f4fc67783471545f0764708565ffaeb99
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-04 10:20:05 +0500

    wrote binarySearch algorithm

---
+++ Interview/Algorthms/Searching.cs
@@ -0,0 +1,19 @@
+﻿namespace Interview.Algorthms;
+
+public class Searching
+{
+    public int binarySearch(int[] arr, int target, int left, int right)
+    {
+        var middle = left + (right - left) / 2;
+        if (left > right)
+            return -1;
+
+        if (arr[middle] == target)
+            return middle;
+
+        else if (arr[middle] > target)
+            return binarySearch(arr, target, left, middle - 1);
+        else
+            return binarySearch(arr, target, middle + 1, right);
+    }
+}
--- Interview/Program.cs
+++ Interview/Program.cs
@@ -6,5 +6,12 @@ foreach (var item in sorting.bubbleSort(new int[] { 10, 200, 30, 41, 52, 63, 7, 
     Console.WriteLine(item);
 foreach (var item in sorting.selectionSort(new int[] { 10, 200, 30, 41, 52, 63, 7, 8, 93 }))
     Console.WriteLine(item);
-foreach (var item in sorting.insertionSort(new int[] { 10, 200, 30, 41, 52, 63, 7, 8, 93 }))
+foreach (var item in sorting.insertionSort(new int[] { 10, 200, 30, 41, 52, 63, 7, 8, 93, 0 }))
     Console.WriteLine(item);
+
+Searching searching = new Searching();
+
+var sorted = new int[] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+var target = 11;
+
+Console.WriteLine($"Index of {target} is :" + searching.binarySearch(sorted, target, 0, 9));
